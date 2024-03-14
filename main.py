af2e4e03515f296036e785abacfb031881a14be6
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-14 12:23:55 +0000

    Creating block object

--- main.py
+++ main.py
@@ -45,6 +45,14 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     return all_sprites
 
 
+def get_block(size):
+    path = join("assets", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surace((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size) # the 96 needs to be the top left of the corner
+    surface.blit(image, (0,0), rect)
+    return pygame.transform.scale2x(surface)
+
 
 
 class Player(pygame.sprite.Sprite):
@@ -54,6 +62,7 @@ class Player(pygame.sprite.Sprite):
     ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
@@ -104,6 +113,27 @@ class Player(pygame.sprite.Sprite):
     def draw(self, win):
 
         win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 def get_background(name):
