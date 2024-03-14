2732463a05ff12776f3a585c7d5efe24e4f21cd3
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-14 12:45:35 +0000

    Adding collision

--- main.py
+++ main.py
@@ -94,6 +94,15 @@ class Player(pygame.sprite.Sprite):
         self.fall_count += 1
         self.update_sprite()
 
+    def landed(self):
+        self.fall_count = 0
+        self.y_vel = 0
+        self.jump_count = 0
+
+    def hit_head(self):
+        self.count =0
+        self.y_vel += -1
+
     def update_sprite(self):
         sprite_sheet = "idle"
         if self.x_vel != 0:
@@ -162,7 +171,21 @@ def draw(window, background, bg_image, player, objects):
     pygame.display.update()
 
 
-def handle_move(player):
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+        collided_objects.append(obj)
+    return collided_objects
+
+
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
@@ -170,6 +193,8 @@ def handle_move(player):
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
+
+    handle_vertical_collision(player, objects, player.y_vel)
 
 
 def main(window):
@@ -192,7 +217,7 @@ def main(window):
                 run = False
                 break
         player.loop(FPS)
-        handle_move(player)
+        handle_move(player, floor)
         draw(window, background, bg_image, player, floor)
     pygame.quit()
     quit()
